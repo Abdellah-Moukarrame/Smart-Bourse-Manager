8a9127703b0457a4a56eeff4c5ed10c09c08e0c0
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-01-06 15:32:40 +0100

    add config de db

---
+++ Config/config.php
@@ -0,0 +1,17 @@
+<?php
+// config/db.php
+
+$host = 'localhost';
+$dbname = 'smartbourseplanner';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    // Set PDO error mode to exception
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // Optional: fetch associative arrays by default
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
